5f3c780c1e5de2d00861d72039f04f49909a5c65
Christoph Frick <cf@ofnir.net>
2017-10-17 13:27:24 +0200

    Depend on slf4j-simple to prevent warnings on startup

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject wrepl.pomegranate "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [integrant "0.6.1"]
-                 [com.cemerick/pomegranate "0.4.0"]])
+                 [com.cemerick/pomegranate "0.4.0"]
+                 [org.slf4j/slf4j-simple "1.7.25"]])
