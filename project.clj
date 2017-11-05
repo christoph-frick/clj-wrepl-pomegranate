41373c920ccb25f32f783621338bcb0d45be1a2c
Christoph Frick <cf@ofnir.net>
2017-11-05 07:03:50 +0100

    Own group id; dependency updates

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject wrepl.pomegranate "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+(defproject net.ofnir/wrepl.pomegranate "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
                  [integrant "0.6.1"]
-                 [com.cemerick/pomegranate "0.4.0"]
+                 [com.cemerick/pomegranate "1.0.0"]
                  [org.slf4j/slf4j-simple "1.7.25"]])
