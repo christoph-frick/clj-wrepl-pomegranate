0eede6b752111a219ba8ee5b4d9e4a4195e0f56e
Christoph Frick <cf@ofnir.net>
2019-06-07 07:24:05 +0200

    Update to Clojure 1.10.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.ofnir/wrepl.pomegranate "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [integrant "0.7.0"]
                  [com.cemerick/pomegranate "1.1.0"]
                  [org.slf4j/slf4j-simple "1.7.25"]])
