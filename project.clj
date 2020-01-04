4b53120c11ce66d1ecd07fd741669a0c1e33d716
Christoph Frick <cf@ofnir.net>
2020-01-04 13:53:15 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [integrant "0.7.0"]
                  [com.cemerick/pomegranate "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.25"]])
+                 [org.slf4j/slf4j-simple "1.7.29"]])
