2fe4131bb6b67363da802b77b3c217371f93dd57
Christoph Frick <cf@ofnir.net>
2020-02-11 12:28:52 +0100

    Prepare for release

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [integrant "0.8.0"]
                  [com.cemerick/pomegranate "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.30"]])
+                 [org.slf4j/slf4j-simple "1.7.30"]]
+  :deploy-repositories [["releases" :clojars]])
