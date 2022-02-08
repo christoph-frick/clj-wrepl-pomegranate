f36bf4a25fc88c38a3291e00dad0487394bc7bff
Christoph Frick <cf@ofnir.net>
2022-02-08 17:23:40 +0100

    Switch to explicit clojars reference

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
                  [integrant "0.8.0"]
                  [clj-commons/pomegranate "1.2.1"]
                  [org.slf4j/slf4j-simple "1.7.36"]]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
