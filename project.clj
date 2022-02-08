c3bef0ad987a06fb9be5d38bbe3dcc1c32350ff8
Christoph Frick <cf@ofnir.net>
2022-02-08 17:19:07 +0100

    Update versions; switch to new coordinate for pomegranate

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.ofnir/wrepl.pomegranate "0.1.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [integrant "0.8.0"]
-                 [com.cemerick/pomegranate "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.30"]]
+                 [clj-commons/pomegranate "1.2.1"]
+                 [org.slf4j/slf4j-simple "1.7.36"]]
   :deploy-repositories [["releases" :clojars]])
